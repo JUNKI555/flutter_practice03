3cd378ca9cbb00357a9c83beb61051e3ddb8400b
JUNKI555 <junki555@gmail.com>
2020-10-29 15:46:11 +0900

    :construction: Adjustment button postion

--- lib/main.dart
+++ lib/main.dart
@@ -145,74 +145,81 @@ class _NextState extends State<Next> {
       appBar: new AppBar(
         title: const Text('Next'),
       ),
-      body: new Center(
-        child: new Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 24,
-            ),
-            new Text('Launch by ${ModalRoute.of(context).settings.arguments}'),
-            const SizedBox(
-              height: 24,
-            ),
-            new RaisedButton(
-              child: const Text('Launch Next Screen'),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/next', arguments: '/next');
-              },
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            new RaisedButton(
-              child: const Text('Back to Home'),
-              onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('/home'));
-              },
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            new RaisedButton(
-              child: const Text('Logout'),
-              onPressed: () {
-                showDialog<bool>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return new AlertDialog(
-                      content: const Text('Do you want logout?'),
-                      actions: <Widget>[
-                        new FlatButton(
-                          child: const Text('No'),
-                          onPressed: () {
-                            // call then false
-                            Navigator.of(context).pop(false);
-                          },
-                        ),
-                        new FlatButton(
-                          child: const Text('Yes'),
-                          onPressed: () {
-                            // call then true
-                            Navigator.of(context).pop(true);
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                ).then<void>((isClose) {
-                  if (!isClose) {
-                    return;
-                  }
+      body: new Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(60),
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(
+                height: 24,
+              ),
+              new Text(
+                  'Launch by ${ModalRoute.of(context).settings.arguments}'),
+              const SizedBox(
+                height: 24,
+              ),
+              new RaisedButton(
+                child: const Text('Launch Next Screen'),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/next', arguments: '/next');
+                },
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              new RaisedButton(
+                child: const Text('Back to Home'),
+                onPressed: () {
+                  Navigator.popUntil(context, ModalRoute.withName('/home'));
+                },
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              new RaisedButton(
+                child: const Text('Logout'),
+                onPressed: () {
+                  showDialog<bool>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return new AlertDialog(
+                        content: const Text('Do you want logout?'),
+                        actions: <Widget>[
+                          new FlatButton(
+                            child: const Text('No'),
+                            onPressed: () {
+                              // call then false
+                              Navigator.of(context).pop(false);
+                            },
+                          ),
+                          new FlatButton(
+                            child: const Text('Yes'),
+                            onPressed: () {
+                              // call then true
+                              Navigator.of(context).pop(true);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  ).then<void>((isClose) {
+                    if (!isClose) {
+                      return;
+                    }
 
-                  Navigator.pushAndRemoveUntil<dynamic>(
-                      context,
-                      new MaterialPageRoute<dynamic>(
-                          builder: (context) => new Splash()),
-                      (_) => false);
-                });
-              },
-            ),
-          ],
+                    Navigator.pushAndRemoveUntil<dynamic>(
+                        context,
+                        new MaterialPageRoute<dynamic>(
+                            builder: (context) => new Splash()),
+                        (_) => false);
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
